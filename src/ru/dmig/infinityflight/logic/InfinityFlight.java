15527ad2907767feb7616a95f96291f7dbfdcb22
Dmig <Dmig@dimann>
2017-01-29 13:32:20 +0200

    Flight duration and chance of category fligh duration

--- src/ru/dmig/infinityflight/logic/InfinityFlight.java
+++ src/ru/dmig/infinityflight/logic/InfinityFlight.java
@@ -10,6 +10,25 @@ package ru.dmig.infinityflight.logic;
  * @author Dmig
  */
 public final class InfinityFlight {
+    
+    /**
+     * Flight duration between stations;
+     * FD[0] - low flight duration
+     * FD[1] - normal
+     * FD[2] - high
+     * 
+     * FD[x][0] - minimum
+     * FD[x][1] - maximum.
+     */
+    public static final short[][] FLIGHT_DURATION = {{60,70},{80,90},{100-120}};
+    
+    /**
+     * Chances of flight duration categories:
+     * CFD[0] - low flight duration
+     * CFD[1] - normal
+     * CFD[2] - high.
+     */
+    public static final byte[] CHANCES_FLIGHT_DURATION = {19,71,10};
 
     /**
      * @param args the command line arguments
