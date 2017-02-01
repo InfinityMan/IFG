c74d25aae2d75a1fc92101cd4b21696f936ce7e9
Dmig <Dmig@dimann>
2017-02-01 18:11:13 +0200

    Updater

---
+++ src/ru/dmig/infinityflight/logic/Updater.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.dmig.infinityflight.logic;
+
+/**
+ *
+ * @author Dmig
+ */
+public final class Updater extends Thread {
+    
+    public Ship ship;
+    private boolean ended = false;
+
+    public Updater(Ship ship) {
+        this.ship = ship;
+    }
+
+    @Override
+    public void run() {
+        while(!ended) {
+            try {
+                Thread.sleep(1000);
+                ship.updateHour();
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+                System.exit(1);
+            }
+        }
+    }
+    
+    public void end() {
+        ended = true;
+    }
+    
+}
