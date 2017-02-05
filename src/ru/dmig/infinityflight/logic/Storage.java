e4d36f3e64bce7b4ae7e84117245d9a4089169bf
Dmig <Dmig@dimann>
2017-02-05 11:51:30 +0200

    Storage class

---
+++ src/ru/dmig/infinityflight/logic/Storage.java
@@ -0,0 +1,186 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.dmig.infinityflight.logic;
+
+/**
+ * Class for storages on ship and stations
+ *
+ * @author Dmig
+ */
+public final class Storage {
+
+    private int foodAmount;
+    private int maxFoodAmount;
+
+    private float fuelAmount;
+    private float maxFuelAmount;
+
+    private short medicineAmount;
+    private short maxMedicineAmount;
+
+    private short spareAmount;
+    private short maxSpareAmount;
+
+    public Storage(int foodAmount, int maxFoodAmount, float fuelAmount, float maxFuelAmount, short medicineAmount, short maxMedicineAmount, short spareAmount, short maxSpareAmount) {
+        this.foodAmount = foodAmount;
+        this.maxFoodAmount = maxFoodAmount;
+        this.fuelAmount = fuelAmount;
+        this.maxFuelAmount = maxFuelAmount;
+        this.medicineAmount = medicineAmount;
+        this.maxMedicineAmount = maxMedicineAmount;
+        this.spareAmount = spareAmount;
+        this.maxSpareAmount = maxSpareAmount;
+    }
+    
+    //<editor-fold defaultstate="collapsed" desc="Get and set">
+    
+    /**
+     * Get the value of foodAmount
+     *
+     * @return the value of foodAmount
+     */
+    public int getFoodAmount() {
+        return foodAmount;
+    }
+    
+    /**
+     * Set the value of foodAmount
+     *
+     * @param foodAmount new value of foodAmount
+     */
+    public void setFoodAmount(int foodAmount) {
+        this.foodAmount = foodAmount;
+    }
+    
+    /**
+     * Get the value of maxFoodAmount
+     *
+     * @return the value of maxFoodAmount
+     */
+    public int getMaxFoodAmount() {
+        return maxFoodAmount;
+    }
+    
+    /**
+     * Set the value of maxFoodAmount
+     *
+     * @param maxFoodAmount new value of maxFoodAmount
+     */
+    public void setMaxFoodAmount(int maxFoodAmount) {
+        this.maxFoodAmount = maxFoodAmount;
+    }
+    
+    /**
+     * Get the value of fuelAmount
+     *
+     * @return the value of fuelAmount
+     */
+    public float getFuelAmount() {
+        return fuelAmount;
+    }
+    
+    /**
+     * Set the value of fuelAmount
+     *
+     * @param fuelAmount new value of fuelAmount
+     */
+    public void setFuelAmount(float fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+    
+    /**
+     * Get the value of maxFuelAmount
+     *
+     * @return the value of maxFuelAmount
+     */
+    public float getMaxFuelAmount() {
+        return maxFuelAmount;
+    }
+    
+    /**
+     * Set the value of maxFuelAmount
+     *
+     * @param maxFuelAmount new value of maxFuelAmount
+     */
+    public void setMaxFuelAmount(float maxFuelAmount) {
+        this.maxFuelAmount = maxFuelAmount;
+    }
+    
+    /**
+     * Get the value of medicineAmount
+     *
+     * @return the value of medicineAmount
+     */
+    public short getMedicineAmount() {
+        return medicineAmount;
+    }
+    
+    /**
+     * Set the value of medicineAmount
+     *
+     * @param medicineAmount new value of medicineAmount
+     */
+    public void setMedicineAmount(short medicineAmount) {
+        this.medicineAmount = medicineAmount;
+    }
+    
+    /**
+     * Get the value of maxMedicineAmount
+     *
+     * @return the value of maxMedicineAmount
+     */
+    public short getMaxMedicineAmount() {
+        return maxMedicineAmount;
+    }
+    
+    /**
+     * Set the value of maxMedicineAmount
+     *
+     * @param maxMedicineAmount new value of maxMedicineAmount
+     */
+    public void setMaxMedicineAmount(short maxMedicineAmount) {
+        this.maxMedicineAmount = maxMedicineAmount;
+    }
+    
+    /**
+     * Get the value of spareAmount
+     *
+     * @return the value of spareAmount
+     */
+    public short getSpareAmount() {
+        return spareAmount;
+    }
+    
+    /**
+     * Set the value of spareAmount
+     *
+     * @param spareAmount new value of spareAmount
+     */
+    public void setSpareAmount(short spareAmount) {
+        this.spareAmount = spareAmount;
+    }
+    
+    /**
+     * Get the value of maxSpareAmount
+     *
+     * @return the value of maxSpareAmount
+     */
+    public short getMaxSpareAmount() {
+        return maxSpareAmount;
+    }
+    
+    /**
+     * Set the value of maxSpareAmount
+     *
+     * @param maxSpareAmount new value of maxSpareAmount
+     */
+    public void setMaxSpareAmount(short maxSpareAmount) {
+        this.maxSpareAmount = maxSpareAmount;
+    }
+    
+    //</editor-fold>
+    
+}
